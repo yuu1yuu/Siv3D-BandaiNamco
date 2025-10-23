a8f3fe6220c046ee0e0656b9e56fe4ecb60d7f0a
Giza10syouwa <it242224@trident.ac.jp>
2025-10-23 12:37:18 +0900

    テスト

--- Game/Scene/GameScene.cpp
+++ Game/Scene/GameScene.cpp
@@ -17,4 +17,6 @@ void Game::GameScene::Draw() const
 {
 	FontAsset(U"GameFont")(U"Game Scene").drawAt(Scene::Center(), Palette::Green);
 	FontAsset(U"MenuFont")(U"[Space] Go to Result").drawAt(Scene::Center().movedBy(0, 60), Palette::Yellow);
+	FontAsset(U"MenuFont")(U"たかちゃ").drawAt(Scene::Center().movedBy(0, 90), Palette::Yellow);
+
 }
